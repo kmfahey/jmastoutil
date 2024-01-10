51d4bb1229eb9321df7cfd8f6099b256873610b6
Kerne Fahey <kernefahey@protonmail.com>
2024-01-09 23:12:34 -0800

    added table creation statements as individual private methods

--- JMastoUtil/src/main/java/org/kmfahey/jmastoutil/model/DataStore.java
+++ JMastoUtil/src/main/java/org/kmfahey/jmastoutil/model/DataStore.java
@@ -82,4 +82,157 @@ public class DataStore {
 
         return appDirPath.resolve(databaseName);
     }
+
+    private void enableForeignKeys() {
+        try (Statement statement = dbConnection.createStatement()) {
+            statement.execute("PRAGMA foreign_keys = ON;");
+        } catch (SQLException exception) {
+            System.err.println("Unable to enable foreign keys due to SQL error:");
+            exception.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void createProfilesTable() {
+        try (Statement statement = dbConnection.createStatement()) {
+            statement.execute("""
+                CREATE TABLE profiles (
+                    user_id TEXT PRIMARY KEY NOT NULL,
+                    fts_rowid INT,
+                    acct_id INT NOT NULL,
+                    user_name TEXT NOT NULL,
+                    instance TEXT NOT NULL,
+                    uri TEXT NOT NULL,
+                    field_name_1 TEXT,
+                    field_value_1 TEXT,
+                    field_name_2 TEXT,
+                    field_value_2 TEXT,
+                    field_name_3 TEXT,
+                    field_value_3 TEXT,
+                    field_name_4 TEXT,
+                    field_value_4 TEXT,
+                    profile_text TEXT NOT NULL,
+                    earliest_notif DATETIME NOT NULL,
+                    loginable BOOLEAN NOT NULL,
+                    tested BOOLEAN NOT NULL
+                );
+            """);
+        } catch (SQLException exception) {
+            System.err.println("Unable to create profiles table due to SQL error:");
+            exception.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void createProfilesIndex() {
+        try (Statement statement = dbConnection.createStatement()) {
+            statement.execute("CREATE INDEX idx_profiles_fts_rowid ON profiles (fts_rowid);");
+        } catch (SQLException exception) {
+            System.err.println("Unable to add index to profiles table for `fts_rowid` column due to SQL error:");
+            exception.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void createProfilesFtsTable() {
+        try (Statement statement = dbConnection.createStatement()) {
+            statement.execute("""
+                CREATE VIRTUAL TABLE profiles_fts USING fts5(
+                    user_id,
+                    user_name,
+                    instance,
+                    field_name_1,
+                    field_value_1,
+                    field_name_2,
+                    field_value_2,
+                    field_name_3,
+                    field_value_3,
+                    field_name_4,
+                    field_value_4,
+                    profile_text
+                );
+            """);
+        } catch (SQLException exception) {
+            System.err.println("Unable to create virtual table profiles_fts due to SQL error:");
+            exception.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void createNotifsTable() {
+        try (Statement statement = dbConnection.createStatement()) {
+            statement.execute("""
+                CREATE TABLE notifs (
+                    from_user_id TEXT PRIMARY KEY NOT NULL,
+                    to_user_id TEXT NOT NULL,
+                    created_at DATETIME NOT NULL,
+                    notif_type TEXT NOT NULL,
+                    status_uri TEXT,
+                    FOREIGN KEY (from_user_id) REFERENCES profiles(user_id),
+                    FOREIGN KEY (to_user_id) REFERENCES profiles(user_id)
+                );
+            """);
+        } catch (SQLException exception) {
+            System.err.println("Unable to create notifs table due to SQL error:");
+            exception.printStackTrace();
+            System.exit(1);
+        }
+
+    }
+
+    private void createFollowTable() {
+        try (Statement statement = dbConnection.createStatement()) {
+            statement.execute("""
+                CREATE TABLE follow (
+                    by_user_id TEXT PRIMARY KEY NOT NULL,
+                    of_user_id TEXT NOT NULL,
+                    last_event DATETIME NOT NULL,
+                    relation TEXT NOT NULL,
+                    FOREIGN KEY (by_user_id) REFERENCES profiles(user_id),
+                    FOREIGN KEY (of_user_id) REFERENCES profile(user_id)
+                );
+            """);
+        } catch (SQLException exception) {
+            System.err.println("Unable to create follow table due to SQL error:");
+            exception.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private int getLastInsertRowid() {
+        try (
+            Statement statement = dbConnection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT last_insert_rowid();")
+        ) {
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            } else {
+                return -1;
+            }
+        } catch (SQLException exception) {
+            System.err.println("Unable to fetch rowid from last insert into profiles_fts due to SQL error:");
+            exception.printStackTrace();
+            System.exit(1);
+        }
+        return -1;
+    }
+
+    private boolean updateProfilesTableWithRowid(String userId) {
+        try (Statement statement = dbConnection.createStatement()) {
+            int rowsAffected = statement.executeUpdate(
+            "    UPDATE profiles" +
+            "    SET fts_rowid = (" +
+            "        SELECT rowid" +
+            "        FROM profiles_fts" +
+            "        WHERE profiles_fts.user_id = \"" + userId + "\"" +
+            "    )" +
+            "    WHERE user_id = \"" + userId + "\";");
+            return rowsAffected > 0;
+        } catch (SQLException exception) {
+            System.err.println("Unable to update profiles table with rowid from profiles_fts table for user_id '" + userId + "' due to SQL error:");
+            exception.printStackTrace();
+            System.exit(1);
+        }
+        return false;
+    }
 }
